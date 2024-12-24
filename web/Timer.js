dc6da4f7272f9247ed821cd7e91bedb3aa252188
yanghan250816 <528991969@qq.com>
2024-12-24 12:30:06 +0800

    Add files via upload

---
+++ web/Timer.js
@@ -0,0 +1,17 @@
+class Timer{
+    constructor() {
+        this.startTime = Date.now()
+        this.currentTime = this.startTime
+        setInterval(this.updateTime.bind(this),0)
+    }
+
+    // 更新当前时间
+    updateTime(){
+        this.currentTime = Date.now() - this.startTime
+    }
+
+    // 获取当前时间
+    getTime(){
+        return this.currentTime
+    }
+}
