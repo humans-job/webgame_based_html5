3be6a9cc4e6e1030f7839a13404095342259b25e
yanghan250816 <528991969@qq.com>
2024-12-26 16:22:26 +0800

    ScoreCounter.js
    新的计分器，用这个

--- web/ScoreCounter.js
+++ web/ScoreCounter.js
@@ -4,17 +4,28 @@ class ScoreCounter {
         this.Notesum = 100;   // 音符总数（假设是100个音符）
         this.goodsum = 0;   // good情况次数
         this.normalsum = 0; // normal情况次数
+        this.miss = 0;
         this.Scoresum = 0;
     }
 
     count(upperdBoundTime, lowerBoundTime, clickTime){
         // 计数的分类方法（假设是这么个区间）
         let t = (upperdBoundTime + lowerBoundTime)/2;
+        if(clickTime > lowerBoundTime){
+            this.miss++;
+            this.sum()
+            return "miss"
+        }
         if (Math.abs(t- clickTime) < 500){
             this.goodsum++;
+            this.sum()
+            return "good"
         }else{
             this.normalsum++;
+            this.sum()
+            return "normal"
         }
+
     }
 
     sum(){
